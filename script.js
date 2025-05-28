e3152dcb45ae5303d130d792635d8df71c6d3a1b
CodeWithHasan-Aura <hasan1221916108j@gmail.com>
2025-05-28 17:24:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const prompt = input.value;
 const btn = document.getElementById('btn');
 const img = document.getElementById('img');
 function generateImage(){
-    fetch(`https://api.unsplash.com/photos/random?query=${prompt}&client_id=JJp7f5hwz7VqsgqbDGoQ_ukwUVWlidIYlYh1Dzoby60`)
+    fetch(`https://api.unsplash.com/photos/random?query=car&client_id=JJp7f5hwz7VqsgqbDGoQ_ukwUVWlidIYlYh1Dzoby60`)
       .then(res => res.json())
       .then(data => {
         console.log(data);
