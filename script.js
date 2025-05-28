b4cc4721b4be5e56f9666b652b3b38595a429187
CodeWithHasan-Aura <hasan1221916108j@gmail.com>
2025-05-28 13:23:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
+
 const input = document.getElementById('input');
 const prompt = input.value;
-// console.log(prompt);
-
-async function generateImage() {
-  const url = 'https://ai-image-generator3.p.rapidapi.com/generate';
-  const options = {
-    method: 'POST',
-    headers: {
-      'x-rapidapi-key': 'fa4e660511msh18c4e36ba9f5fe0p1d329cjsn535cf42b9da2',
-      'x-rapidapi-host': 'ai-image-generator3.p.rapidapi.com',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      prompt: `${prompt}`,
-      page: 1
-    })
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    return result;
-    // console.log(result.results.images[0]);
-  } catch (error) {
-    console.error(error);
-  }
+const btn = document.getElementById('btn');
+const img = document.getElementById('img');
+function generateImage(){
+    fetch(`https://api.unsplash.com/photos/random?query=${prompt}&client_id=JJp7f5hwz7VqsgqbDGoQ_ukwUVWlidIYlYh1Dzoby60`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        img.src = data.urls.regular;
+      });
 }
 
-const img = document.getElementById('img');
-const btn = document.getElementById('btn');
+btn.addEventListener('click', ()=>{
+ //  console.log(prompt) 
+    generateImage();
+})
+
 const clr = document.getElementById('clr');
 
-btn.addEventListener('click', () => {
-  img.style.display = "block";
-  generateImage().then(result => {
-    img.src = result.results.images[0];
-  }).catch(error => {
-    console.error("Error generating image:", error);
-  });
-});
 clr.addEventListener('click', () => {
   img.style.display = "none";
   input.value = "";
